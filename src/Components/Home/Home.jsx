8e327f735a38716ca419ba9c69b64b98fdcf5d22
codemohitpra03 <codemohitprakanojia2103@gmail.com>
2024-03-27 01:21:56 +0530

    containerized the app

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
-
+import { redirect } from "react-router-dom";
 const Home = () => {
+  
+  
   return (
     <div className="grid grid-cols-[250px_1fr]">
       	<Sidebar/>
